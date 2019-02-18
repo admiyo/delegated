f5314d91a37ba4dd5ea4ef5f2226a38d9af1ef64
Adam Young <ayoung@redhat.com>
2019-02-18 00:32:13 -0500

    use getopts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+
 mod hosts;
 
+extern crate getopts;
+use getopts::Options;
 use std::env;
 use std::fs;
 
@@ -9,18 +12,54 @@ fn words_by_line<'a>(s: &'a str) -> Vec<Vec<&'a str>> {
     }).collect()
 }
 
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+
 fn main() {
+    
+    let mut opts = Options::new();
+    opts.optopt("o", "operation", "operation to perform", "list, set, delete");
+    opts.optflag("h", "help", "print this help menu");
+    opts.optopt("f", "filename", "file to read and write","/etc/hosts");
+    opts.optopt("a", "address", "ipaddress, either v4 or v6", "10.10.2.1");
+
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let _operation = &args[2];
+
+    
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        let program = args[0].clone();
+        print_usage(&program, opts);
+        return;
+    }    
+    let operation: String;
+    let filename: String;
+
+    match matches.opt_str("o") {
+        Some(x) => operation = x,
+        None => operation = "list".to_string()
+    }
+
+    match matches.opt_str("f"){
+        Some(x) => filename = x,
+        None => filename = "./hosts".to_string()
+    }
+    
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-
+    
     let wbyl: std::vec::Vec<std::vec::Vec<&str>> = words_by_line(&contents);
-
-    if _operation == "list" {
+    
+    if operation == "list" {
         hosts::list(wbyl);
-    } else if _operation == "set" {
+    } else if operation == "set" {
 
         let ipaddr = &args[3];
         let hostname = &args[4];
@@ -29,3 +68,4 @@ fn main() {
         println!("Unknown Operation");
     }
 }
+
