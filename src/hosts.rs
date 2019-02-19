7d76cbbefa4f0f2ea2a66d2352f4323dfefcee9b
Adam Young <ayoung@redhat.com>
2019-02-19 16:59:13 -0500

    list returns string now
    added sample data for list test
    check list output in unit tests

--- src/hosts.rs
+++ src/hosts.rs
@@ -7,16 +7,21 @@ impl <'a> Manager<'a>{
     pub fn new (contents: &'a str) -> Manager {
         Manager{
             wbyl: words_by_line(&contents)
-        } 
+        }
     }
 
-    pub fn list(&'a self){
+    pub fn list(&'a self) -> String{
+        let mut out = String::new();
         for i in &self.wbyl {
-            for j in i{
-                print!("{} ", j);
+            for j in i {
+                out.push_str(&format!("{} ", j));
             }
-            println!("");
+            if out.ends_with(" "){
+                out = out.trim().to_string();
+            }
+            out.push_str("\n");
         }
+        out
     }
 
 
@@ -45,15 +50,27 @@ pub fn words_by_line<'a>(s: &'a str) -> Vec<Vec<&'a str>> {
 
 #[cfg(test)]
 mod tests {
-     use super::*;
+    use super::*;
+
     #[test]
     fn test_empty_list() {
         let  contents = "";
         let manager = Manager{
             wbyl: words_by_line(&contents)
         };
-        manager.list();
+        let out = manager.list();
+        assert_eq!(out,"");
     }
+    #[test]
+    fn test_sample_list() {
+        let contents = SAMPLEDATA;
+        let manager = Manager{
+            wbyl: words_by_line(&contents)
+        };
+        let out = manager.list();
+        assert_eq!(out,SAMPLEDATA);
+    }
+
 
     #[test]
     fn test_empty_add() {
@@ -61,4 +78,19 @@ mod tests {
         let manager = Manager::new(&contents);
         manager.add("127.0.0.0", "slashdot.org");
     }
+
+
+    const SAMPLEDATA: &'static str =
+"127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
+
+10.0.0.112 ayoungP50
+10.0.0.104 dialga.home.younglogic.net dialga
+10.0.0.30 munchlax.home.younglogic.net munchlax
+10.127.0.3 passimian passimian.home.younglogic.net
+10.127.0.7 diglett diglett.home.younglogic.net
+
+10.127.0.10 idrac-zygarde
+10.127.0.11 idrac-umbreon
+10.127.0.12 idrac-zubat
+";
 }
