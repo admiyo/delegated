b6f89f6c1f7d243631856fde0220fa2fb1d805a4
Adam Young <ayoung@redhat.com>
2019-02-21 11:11:56 -0500

    print output to screen

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,14 @@ fn print_usage(program: &str, opts: Options) {
 }
 
 fn main() {
-    
+
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
     opts.optopt("f", "hostfile", "file to read and write","/etc/hosts");
     opts.optopt("d", "directory", "directory from which to read changes", "");
 
     let args: Vec<String> = env::args().collect();
-    
+
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
@@ -29,20 +29,21 @@ fn main() {
         let program = args[0].clone();
         print_usage(&program, opts);
         return;
-    }    
+    }
     let filename: String;
-
     match matches.opt_str("f"){
         Some(x) => filename = x,
         None => filename = "./hosts".to_string()
     }
-    
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
-
-    
     let manager = hosts::Hosts::new(contents.to_string());
-        manager.list();
 
+    let dir: String;
+    match matches.opt_str("d"){
+        Some(x) => dir = x,
+        None => dir = "/var/lib/hosts".to_string()
+    }
+
+    print!("{}", manager.list());
 }
-
