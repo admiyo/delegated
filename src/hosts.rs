546b49b928c16bb7c005a93ff97024922c0963bc
Adam Young <ayoung@redhat.com>
2019-02-23 15:05:06 -0500

    Result of running cargo fmt

--- src/hosts.rs
+++ src/hosts.rs
@@ -1,18 +1,16 @@
-use std::io;
+use linked_hash_map::LinkedHashMap;
 use std::fs;
+use std::io;
 use std::path::Path;
 use yaml_rust::Yaml;
 use yaml_rust::YamlLoader;
-use linked_hash_map::LinkedHashMap;
-
 
 pub struct Hosts {
-    lines: Vec<Vec<String>>
+    lines: Vec<Vec<String>>,
 }
 
-impl Hosts{
-
-    pub fn new (contents:  String) -> Hosts {
+impl Hosts {
+    pub fn new(contents: String) -> Hosts {
         let l = words_by_line(&contents);
 
         let mut lines = Vec::new();
@@ -23,18 +21,16 @@ impl Hosts{
             }
             lines.push(line);
         }
-        Hosts{
-            lines: lines
-        }
+        Hosts { lines: lines }
     }
 
-    pub fn list(& self) -> String{
+    pub fn list(&self) -> String {
         let mut out: String = String::new();
         for i in &self.lines {
             for j in i {
                 out.push_str(&format!("{} ", j));
             }
-            if out.ends_with(" "){
+            if out.ends_with(" ") {
                 out = out.trim().to_string();
             }
             out.push_str("\n");
@@ -42,25 +38,25 @@ impl Hosts{
         out.clone()
     }
 
-
-    pub fn add(& mut self, ipaddr:  &str, hostname:  &str){
+    pub fn add(&mut self, ipaddr: &str, hostname: &str) {
         for i in &self.lines {
-            if i.len() == 0{
+            if i.len() == 0 {
                 continue;
             }
-            if i[0] == ipaddr{
-                for j in i{
-                    if j == &hostname{
+            if i[0] == ipaddr {
+                for j in i {
+                    if j == &hostname {
                         //Found it.
-                        return
+                        return;
                     }
                 }
             }
         }
-        self.lines.push(vec![ipaddr.to_string(), hostname.to_string()]);
+        self.lines
+            .push(vec![ipaddr.to_string(), hostname.to_string()]);
     }
 
-    pub fn del(& mut self, ipaddr:  &str, hostname:  &str){
+    pub fn del(&mut self, ipaddr: &str, hostname: &str) {
         let host = vec![ipaddr.to_string(), hostname.to_string()];
         let i = self.lines.iter().position(|x| x == &host);
         match i {
@@ -68,61 +64,54 @@ impl Hosts{
             None => Vec::new(),
         };
     }
-
-    
 }
 
 pub fn words_by_line<'a>(s: &'a str) -> Vec<Vec<&'a str>> {
-    s.lines().map(|line| {
-        line.split_whitespace().collect()
-    }).collect()
+    s.lines()
+        .map(|line| line.split_whitespace().collect())
+        .collect()
 }
 
-
-
-
-fn add_host(map:  &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>,
-            hosts: &mut Hosts){
-    let adders = map.get( &Yaml::from_str("add")).unwrap().as_hash().unwrap();
+fn add_host(map: &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>, hosts: &mut Hosts) {
+    let adders = map.get(&Yaml::from_str("add")).unwrap().as_hash().unwrap();
     for (name, address) in adders {
         let n = name.as_str().unwrap();
         let a = address.as_str().unwrap();
         hosts.add(n, a);
     }
-}                   
+}
 
-
-fn del_host(map:  &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>,
-            hosts: &mut Hosts){
-    let deletes = map.get( &Yaml::from_str("del")).unwrap().as_hash().unwrap();
+fn del_host(map: &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>, hosts: &mut Hosts) {
+    let deletes = map.get(&Yaml::from_str("del")).unwrap().as_hash().unwrap();
     for (name, address) in deletes {
         let n = name.as_str().unwrap();
         let a = address.as_str().unwrap();
         hosts.del(n, a);
     }
-}                   
-
+}
 
 // one possible implementation of walking a directory only visiting files
 pub fn visit_dirs(dir: &Path, hosts: &mut Hosts) -> io::Result<()> {
     if dir.is_dir() {
-        for e in fs::read_dir(dir)? {            
+        for e in fs::read_dir(dir)? {
             let entry = e?;
             let path = entry.path();
             let s: String;
-            match path.to_str(){
+            match path.to_str() {
                 Some(x) => s = x.to_string(),
-                None => s = String::new()
+                None => s = String::new(),
             }
             if path.is_dir() {
                 visit_dirs(&path, hosts)?;
             } else {
-                if s.ends_with("yml"){
-                    println!("{}",s);
-                    let contents = fs::read_to_string(path)
-                        .expect("Something went wrong reading the file");
-                    let docs: std::vec::Vec<yaml_rust::Yaml> = YamlLoader::load_from_str(&contents).unwrap();
-                    let map:  &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>  = &docs[0].as_hash().unwrap();
+                if s.ends_with("yml") {
+                    println!("{}", s);
+                    let contents =
+                        fs::read_to_string(path).expect("Something went wrong reading the file");
+                    let docs: std::vec::Vec<yaml_rust::Yaml> =
+                        YamlLoader::load_from_str(&contents).unwrap();
+                    let map: &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml> =
+                        &docs[0].as_hash().unwrap();
                     add_host(map, hosts);
                     del_host(map, hosts);
                 }
@@ -132,18 +121,13 @@ pub fn visit_dirs(dir: &Path, hosts: &mut Hosts) -> io::Result<()> {
     Ok(())
 }
 
-
-
-pub fn run(dir: &str, filename: &str){
+pub fn run(dir: &str, filename: &str) {
     let dir_path = Path::new(dir);
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let mut hosts = Hosts::new(contents.to_string());
     visit_dirs(dir_path, &mut hosts).unwrap();
     print!("{}", hosts.list());
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -154,19 +138,19 @@ mod tests {
 
     #[test]
     fn test_empty_list() {
-        let  contents = "".to_string();
+        let contents = "".to_string();
         let manager = Hosts::new(contents);
         let out = manager.list();
-        assert_eq!(out,"");
+        assert_eq!(out, "");
     }
+
     #[test]
     fn test_sample_list() {
         let contents = SAMPLEDATA.to_string();
         let manager = Hosts::new(contents);
         let out = manager.list();
-        assert_eq!(out,SAMPLEDATA);
+        assert_eq!(out, SAMPLEDATA);
     }
-
 
     #[test]
     fn test_delete() {
@@ -174,21 +158,19 @@ mod tests {
         let mut manager = Hosts::new(contents);
         let orig = manager.list();
         assert_eq!(orig, "127.0.0.0 slashdot.org\n");
-        manager.del("127.0.0.0","slashdot.org");
+        manager.del("127.0.0.0", "slashdot.org");
         let out = manager.list();
         assert_eq!(out, "");
     }
 
-    
     #[test]
     fn test_empty_add() {
         let contents = "".to_string();
         let mut manager = Hosts::new(contents);
         manager.add("127.0.0.0", "slashdot.org");
         let out = manager.list();
-        assert_eq!(out,"127.0.0.0 slashdot.org\n");
+        assert_eq!(out, "127.0.0.0 slashdot.org\n");
     }
-
 
     #[test]
     fn test_functional() {
@@ -197,16 +179,14 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let contents = fs::read_to_string(TEST_HOSTS_FILE)
-            .expect("Something went wrong reading the file");
+        let contents =
+            fs::read_to_string(TEST_HOSTS_FILE).expect("Something went wrong reading the file");
         let hosts = Hosts::new(contents.to_string());
         hosts.list();
     }
 
-
-    
     const SAMPLEDATA: &'static str =
-"127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
+        "127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
 
 10.0.0.112 ayoungP50
 10.0.0.104 dialga.home.younglogic.net dialga
--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,22 @@ extern crate yaml_rust;
 use getopts::Options;
 use std::env;
 
-
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} FILE [options]", program);
     print!("{}", opts.usage(&brief));
 }
 
 fn main() {
-
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
-    opts.optopt("f", "hostfile", "file to read and write","/etc/hosts");
+    opts.optopt("f", "hostfile", "file to read and write", "/etc/hosts");
     opts.optopt("d", "directory", "directory from which to read changes", "");
 
     let args: Vec<String> = env::args().collect();
 
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
     };
 
     if matches.opt_present("h") {
@@ -32,19 +30,16 @@ fn main() {
         return;
     }
     let filename: String;
-    match matches.opt_str("f"){
+    match matches.opt_str("f") {
         Some(x) => filename = x,
-        None => filename = "./hosts".to_string()
+        None => filename = "./hosts".to_string(),
     }
 
     let dir: String;
-    match matches.opt_str("d"){
+    match matches.opt_str("d") {
         Some(x) => dir = x,
-        None => dir = "/var/lib/hosts".to_string()
+        None => dir = "/var/lib/hosts".to_string(),
     }
 
     hosts::run(&dir, &filename);
-    
 }
-
-
