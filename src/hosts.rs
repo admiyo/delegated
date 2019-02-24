cc5aa7c642c38eb40380be1cafa531fa98737ba3
Adam Young <ayoung@redhat.com>
2019-02-24 18:52:10 -0500

    build domain names from host names and directories

--- src/hosts.rs
+++ src/hosts.rs
@@ -57,6 +57,7 @@ impl Hosts {
     }
 
     pub fn del(&mut self, ipaddr: &str, hostname: &str) {
+        println!("attempting to delete {} {}", ipaddr, hostname);
         let host = vec![ipaddr.to_string(), hostname.to_string()];
         let i = self.lines.iter().position(|x| x == &host);
         match i {
@@ -72,48 +73,57 @@ pub fn words_by_line<'a>(s: &'a str) -> Vec<Vec<&'a str>> {
         .collect()
 }
 
-fn add_host(map: &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>, hosts: &mut Hosts) {
+fn add_host(
+    map: &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>,
+    hosts: &mut Hosts,
+    domain: &str,
+) {
     let adders = map.get(&Yaml::from_str("add")).unwrap().as_hash().unwrap();
     for (name, address) in adders {
         let n = name.as_str().unwrap();
         let a = address.as_str().unwrap();
-        hosts.add(n, a);
+        hosts.add(&format!("{}{}", n, domain), a);
     }
 }
 
-fn del_host(map: &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>, hosts: &mut Hosts) {
+fn del_host(
+    map: &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml>,
+    hosts: &mut Hosts,
+    domain: &str,
+) {
     let deletes = map.get(&Yaml::from_str("del")).unwrap().as_hash().unwrap();
     for (name, address) in deletes {
         let n = name.as_str().unwrap();
         let a = address.as_str().unwrap();
-        hosts.del(n, a);
+        hosts.del(a, &format!("{}{}", n, domain));
     }
 }
 
 // one possible implementation of walking a directory only visiting files
-pub fn visit_dirs(dir: &Path, hosts: &mut Hosts) -> io::Result<()> {
+pub fn visit_dirs(dir: &Path, hosts: &mut Hosts, domain: &str) -> io::Result<()> {
     if dir.is_dir() {
         for e in fs::read_dir(dir)? {
             let entry = e?;
             let path = entry.path();
-            let s: String;
-            match path.to_str() {
-                Some(x) => s = x.to_string(),
-                None => s = String::new(),
+
+            let name: &str;
+            let n = entry.file_name();
+            match n.to_str() {
+                Some(x) => name = x,
+                None => name = "",
             }
             if path.is_dir() {
-                visit_dirs(&path, hosts)?;
+                visit_dirs(&path, hosts, &format!(".{}{}", &name, domain))?;
             } else {
-                if s.ends_with("yml") {
-                    println!("{}", s);
+                if name.ends_with("yml") {
                     let contents =
                         fs::read_to_string(path).expect("Something went wrong reading the file");
                     let docs: std::vec::Vec<yaml_rust::Yaml> =
                         YamlLoader::load_from_str(&contents).unwrap();
                     let map: &&LinkedHashMap<yaml_rust::Yaml, yaml_rust::Yaml> =
                         &docs[0].as_hash().unwrap();
-                    add_host(map, hosts);
-                    del_host(map, hosts);
+                    add_host(map, hosts, domain);
+                    del_host(map, hosts, domain);
                 }
             }
         }
@@ -128,7 +138,7 @@ pub fn load_hosts(filename: &str) -> Hosts {
 
 pub fn run(dir: &str, filename: &str) {
     let mut hosts = load_hosts(filename);
-    visit_dirs(Path::new(dir), &mut hosts).unwrap();
+    visit_dirs(Path::new(dir), &mut hosts, "").unwrap();
     print!("{}", hosts.list());
 }
 
@@ -142,36 +152,49 @@ mod tests {
     #[test]
     fn test_empty_list() {
         let contents = "".to_string();
-        let manager = Hosts::new(contents);
-        let out = manager.list();
+        let hosts = Hosts::new(contents);
+        let out = hosts.list();
         assert_eq!(out, "");
     }
 
     #[test]
     fn test_sample_list() {
         let contents = SAMPLEDATA.to_string();
-        let manager = Hosts::new(contents);
-        let out = manager.list();
-        assert_eq!(out, SAMPLEDATA);
+        let hosts = Hosts::new(contents);
+        let hosts_list = hosts.list();
+        for host in TEST_HOSTS {
+            assert!(hosts_list.contains(host));
+        }
     }
 
     #[test]
-    fn test_delete() {
+    fn test_simple_delete() {
         let contents = "127.0.0.0 slashdot.org\n".to_string();
-        let mut manager = Hosts::new(contents);
-        let orig = manager.list();
+        let mut hosts = Hosts::new(contents);
+        let orig = hosts.list();
         assert_eq!(orig, "127.0.0.0 slashdot.org\n");
-        manager.del("127.0.0.0", "slashdot.org");
-        let out = manager.list();
+        hosts.del("127.0.0.0", "slashdot.org");
+        let out = hosts.list();
         assert_eq!(out, "");
+    }
+
+    #[test]
+    fn test_delete_from_sample_data() {
+        let test_str = "passimian.home.younglogic.net";
+        let contents = SAMPLEDATA.to_string();
+        let mut hosts = Hosts::new(contents);
+        let host_list = hosts.list();
+        assert!(host_list.contains(test_str));
+        hosts.del("10.127.0.3", test_str);
+        assert!(!hosts.list().contains(test_str));
     }
 
     #[test]
     fn test_empty_add() {
         let contents = "".to_string();
-        let mut manager = Hosts::new(contents);
-        manager.add("127.0.0.0", "slashdot.org");
-        let out = manager.list();
+        let mut hosts = Hosts::new(contents);
+        hosts.add("127.0.0.0", "slashdot.org");
+        let out = hosts.list();
         assert_eq!(out, "127.0.0.0 slashdot.org\n");
     }
 
@@ -183,25 +206,36 @@ mod tests {
     #[test]
     fn test_parse() {
         let hosts = load_hosts(TEST_HOSTS_FILE);
-        hosts.list();
+        let hosts_list = hosts.list();
+        for host in TEST_HOSTS {
+            assert!(hosts_list.contains(host));
+        }
     }
 
     #[test]
     fn test_modify() {
         let mut hosts = load_hosts(TEST_HOSTS_FILE);
         assert!(hosts.list().contains("passimian.home.younglogic.net"));
-        visit_dirs(Path::new(TEST_DATA_DIR), &mut hosts).unwrap();
+        visit_dirs(Path::new(TEST_DATA_DIR), &mut hosts, "").unwrap();
         assert!(!hosts.list().contains("passimian.home.younglogic.net"));
     }
+
+    const TEST_HOSTS: &'static [&'static str] = &[
+        "ayoungP50",
+        "dialga.home.younglogic.net",
+        "munchlax.home.younglogic.net",
+        "passimian.home.younglogic.net",
+        "diglett.home.younglogic.net",
+    ];
 
     const SAMPLEDATA: &'static str =
         "127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
 
 10.0.0.112 ayoungP50
-10.0.0.104 dialga.home.younglogic.net dialga
-10.0.0.30 munchlax.home.younglogic.net munchlax
-10.127.0.3 passimian passimian.home.younglogic.net
-10.127.0.7 diglett diglett.home.younglogic.net
+10.0.0.104 dialga.home.younglogic.net
+10.0.0.30  munchlax.home.younglogic.net
+10.127.0.3 passimian.home.younglogic.net
+10.127.0.7 diglett.home.younglogic.net
 
 10.127.0.10 idrac-zygarde
 10.127.0.11 idrac-umbreon
